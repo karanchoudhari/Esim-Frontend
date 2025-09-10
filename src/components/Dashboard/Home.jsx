cb016baa21df03c8375def3280939549ef295234
karanchoudhari <choudharikaran32@gmail.com>
2025-09-10 21:41:31 +0530

    Fix Redux slice filenames and imports to match casing

--- src/components/Dashboard/Home.jsx
+++ src/components/Dashboard/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { checkKYCStatus } from "../../redux/kycSlice";
+import { checkKYCStatus } from "../../redux/KycSlice.js";
 
 import { activeEsim } from "../../redux/esimSlice";
 import './Home.css';
