ac8f9573f0b20ba902273a9b86c117f4a65e392a
karanchoudhari <choudharikaran32@gmail.com>
2025-09-10 21:17:41 +0530

    Fix Redux store imports with correct casing

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import AuthSlice from "./Createuser"; // Single auth slice
-import kycSlice from "./kycSlice";
-import esimSlice from "./esimSlice";
+import AuthSlice from "./Createuser.js"; // exact filename
+import kycSlice from "./kycSlice.js";    // exact filename
+import esimSlice from "./esimSlice.js";  // exact filename
+
 
 export const store = configureStore({
   reducer: {
