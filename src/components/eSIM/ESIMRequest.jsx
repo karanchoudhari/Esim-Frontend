5171540b50594d3d639a773bb64d303904b32c06
karanchoudhari <choudharikaran32@gmail.com>
2025-09-14 13:29:16 +0530

    Fix case-sensitive imports for KycSlice

--- src/components/eSIM/ESIMRequest.jsx
+++ src/components/eSIM/ESIMRequest.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { requestESIM } from '../../redux/esimSlice';
-import { checkKYCStatus } from '../../redux/kycSlice.js';
+import { checkKYCStatus } from '../../redux/KycSlice.js';
 import { useNavigate } from 'react-router-dom';
 import AxiosInstance from "../../../Axiosinstance"
 
